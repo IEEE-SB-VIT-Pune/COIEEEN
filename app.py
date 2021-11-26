214eaa786bdb0598409294cb5c77bdc401ded7e4
chiragvaswani <chiragvaswani751@gmail.com>
2021-11-26 08:35:21 +0530

    Instantiate blockchain

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, request, render_template
 import requests
 from uuid import uuid4
 from urllib.parse import urlparse
-
+from blockchain import Blockchain
+# Create Flask app
 app = Flask(__name__)
+
+# Instantiate an object from the blockchain class
+blockchain = Blockchain()
 
 @app.route('/', methods = ['GET'])
 def default():
