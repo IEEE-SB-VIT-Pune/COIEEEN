bcb1e0ce90f4397277c3c2c6ef1c7c93c9dd0bc7
chiragvaswani <chiragvaswani751@gmail.com>
2021-11-26 09:28:07 +0530

    Use controller to define mine_block_router

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import requests
 from uuid import uuid4
 from urllib.parse import urlparse
 from blockchain import Blockchain
+
+from controllers.blockchain import *
+
 # Create Flask app
 app = Flask(__name__)
-
-# Instantiate an object from the blockchain class
-blockchain = Blockchain()
 
 @app.route('/', methods = ['GET'])
 def default():
     return render_template('index.html')
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block_router():
+    return mine_block()
